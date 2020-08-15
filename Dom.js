52889f38b6d0bc8b2e7e82b783885bd7c9d8c030
unknown <shamimrocky801@yahoo.com>
2020-08-16 01:08:01 +0600

    DOM

---
+++ Dom.js
@@ -0,0 +1,20 @@
+
+
+//dom using addeevnt
+document.getElementById("headerid").addEventListener("click",function(){
+
+document.getElementById("headerid").style.background = "orange";
+
+
+});
+
+
+
+document.getElementById("headerid").onclick=py;
+
+function py(){
+	document.getElementById("headerid").innerHTML="<h1>Shamim</h1>"
+}
+
+
+//console.log(elements);
