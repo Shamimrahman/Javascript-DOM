7c115467e1de044ac8d69eef84cf8feb48dfd5be
MD.Shamim Rahman <43651007+Shamimrahman@users.noreply.github.com>
2020-08-17 02:50:09 +0600

    Add files via upload

---
+++ ClasslistDOm.js
@@ -0,0 +1,11 @@
+// DOM class list
+
+document.getElementById("headerid").addEventListener("click", abc);
+
+function abc ()
+{
+	document.getElementById("headerid").classList.add("xyz"); // aikhan a xyz css er ekti class ja amra add korlam 
+	                                                          //abc class er sathe and amra multiple class add korte pari
+	                                                          //click korle xyz class er function chalu hbe exampole=Bg chng hbe
+
+}
